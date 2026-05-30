f4d775bdf4b8098939986fca92f1580b7c358136
Kishal <kishal2007@gmail.com>
2026-05-30 12:44:34 +0530

    feat(security): harden authentication system and standardize error handling

--- controller/suggestionController.js
+++ controller/suggestionController.js
@@ -1,14 +1,15 @@
 const suggestions = require('../model/suggestionData');
 const services = require('../services.config');
+const asyncHandler = require('../utils/asyncHandler');
 
-exports.getPage = (req, res) => {
+exports.getPage = asyncHandler(async (req, res) => {
   const categories = Object.keys(suggestions);
   res.render('suggestions', { categories, result: null, selected: null, services });
-};
+});
 
-exports.getSuggestions = (req, res) => {
+exports.getSuggestions = asyncHandler(async (req, res) => {
   const { category } = req.body;
   const categories = Object.keys(suggestions);
   const result = suggestions[category] || null;
   res.render('suggestions', { categories, result, selected: category, services });
-};
+});
